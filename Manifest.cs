79256227d3dd2567d6ef3c4357db288911b264ba
Veronica Zibin <veronica_stuff@hotmail.com>
2020-04-24 14:01:29 +0300

    first commit

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Blog Theme Hebrew",
+    Author = "The Veronica",
+    Website = "https://orchardproject.net",
+    Version = "2.0.0",
+    Description = "A theme adapted for hebrew blogs."
+)]
